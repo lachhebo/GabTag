d84d3258e27eac8b1b6148a482a2a73aed0f6400
Ismaël Lachheb <ismael.lachheb@protonmail.com>
2019-05-12 20:30:17 +0200

    cover handling in progress

--- src/MP3Handler.py
+++ src/MP3Handler.py
@@ -1,29 +1,93 @@
 from .audiobasics import AudioBasics
 from mutagen.mp3 import EasyMP3, MP3
+from mutagen.id3 import ID3, APIC, TRCK, USLT
+from PIL import Image
+import io
 
 class MP3Handler(AudioBasics):
 
     def __init__(self,adress):
         self.adress = adress
-        self.audio = EasyMP3(adress)
-        self.tags = self.audio.tags
+        self.audio_easy = EasyMP3(adress)
+        self.audio_cover = MP3(adress,ID3=ID3)
+        self.tags = self.audio_easy.tags
 
 
-    def getTag(self,tag_key):
-        if tag_key in self.tags:
-            return self.tags[tag_key][0]
+    def getTag(self,tag_key): #TODO
+        if tag_key != "cover" :
+            if tag_key in self.tags:
+                #print("GETTAG : ", self.tags[tag_key][0])
+                return self.tags[tag_key][0]
+            else :
+                return ""
         else :
-            return ""
+            id3 = ID3(self.adress)
+            cover_tag = id3.getall('APIC')
+
+            #print("COVERTAG :",cover_tag)
+
+            if len(cover_tag)>0 :
+                return cover_tag[0].data
+            else:
+                return None
+
+
+    def get_extension_mime(self, filename):
+
+         namelist = filename.split('/')
+         return namelist[-1]
+
+
 
     def check_tag_existence(self,key):
-        return key in self.tags
+        if key != "cover":
+            return key in self.tags
+        else :
+            id3 = ID3(self.adress)
+            cover_tag = id3.getall('APIC')
+            return len(cover_tag)>0
 
-    def setTag(self,tag_key,tag_value):
-        self.tags[tag_key] = tag_value
+
+    def setTag(self,tag_key,tag_value): #TODO
+
+        if tag_key == "cover":
+            extension_image  = self.get_extension_image(tag_value)
+
+           # img = Image.open(tag_value)
+
+            #glibbytes = GLib.Bytes.new(img.tobytes())
+
+
+            #imagedata = open(tag_value, 'rb').read()
+            self.audio_cover.tags
+
+            # id3 = ID3(tag_value)
+
+            # if id3.getall('APIC'):
+            #     id3.delall('APIC')
+
+            self.audio_cover.tags.add(
+                APIC(
+                    encoding = 3,  # UTF-8
+                    mime= extension_image,   # '/image/png'
+                    type= 3,  # 3 is for album art
+                    desc='Cover',
+                    data= open(tag_value, 'rb').read()      #img.read()  # Reads and adds album art
+                )
+            )
+        else :
+            self.tags[tag_key] = tag_value
+
+
+    def get_extension_image(self, filename):
+
+         namelist = filename.split('.')
+         return "/image/" + namelist[-1]
 
 
     def savemodif(self):
         self.tags.save(self.adress)
+        self.audio_cover.tags.save(self.adress)
 
 
     
--- src/view.py
+++ src/view.py
@@ -1,20 +1,23 @@
 from gi import require_version
 require_version('Gtk', '3.0')
 
-from gi.repository import Gtk
+from gi.repository import Gtk, GdkPixbuf, GLib
+from PIL import Image
+import io
 
 class View:
 
 
     class __View:
 
-        def __init__(self, tree_view, title, album, artist, genre):
+        def __init__(self, tree_view, title, album, artist, genre, cover):
 
             self.tree_view = tree_view
             self.title = title
             self.album = album
             self.artist = artist
             self.genre = genre
+            self.cover = cover
 
         def erase(self):
             self.genre.set_text("")
@@ -27,6 +30,25 @@ class View:
             self.album.set_text(tagdico["album"]["value"])
             self.title.set_text(tagdico["title"]["value"])
             self.artist.set_text(tagdico["artist"]["value"])
+            if tagdico["cover"]["value"] != None:
+                if len(tagdico["cover"]["value"])>100 :
+                     with  Image.open(io.BytesIO(tagdico["cover"]["value"])) as img :
+
+                        img_resized = img.resize((128, 128))
+                        glibbytes = GLib.Bytes.new(img_resized.tobytes())
+
+                        pixbuf = GdkPixbuf.Pixbuf.new_from_bytes(glibbytes,
+                                                                GdkPixbuf.Colorspace.RGB,
+                                                                False,
+                                                                8,
+                                                                128,
+                                                                128,
+                                                                len(img_resized.getbands())*img_resized.width)
+
+                        self.cover.set_from_pixbuf(pixbuf)
+                else :
+                    pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale(tagdico["cover"]["value"], 128, 128, False)
+                    self.cover.set_from_pixbuf(pixbuf)
 
         def add_column(self, name):
             renderer = Gtk.CellRendererText()
@@ -37,12 +59,12 @@ class View:
 
     __instance = None
 
-    def __init__(self, tree_view, title, album, artist, genre):
+    def __init__(self, tree_view, title, album, artist, genre, cover):
         """ Virtually private constructor. """
         if View.__instance != None:
             raise Exception("This class is a singleton!")
         else:
-            View.__instance = View.__View(tree_view, title, album, artist, genre)
+            View.__instance = View.__View(tree_view, title, album, artist, genre, cover)
 
 
     @staticmethod
